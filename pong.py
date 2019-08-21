cb9b53c806ac28a4bc46a5b98287aff026d78d08
ColeGuillaume <42841490+ColeGuillaume@users.noreply.github.com>
2019-08-21 19:46:18 -0400

    Add files via upload
    Added original Pong.py

---
+++ pong.py
@@ -0,0 +1,64 @@
+#Let's try to make pong
+import turtle
+
+ycord1 = -100
+ycord2 = -100
+
+
+#Set up the screen
+def createBoard(bob,gameScreen,player1,player2,ball):
+    #gameScreen.title("PONG")
+    bob.ht()
+    #Draw Box
+    bob.speed(0)
+    bob.penup()
+    bob.pencolor("Black")
+    bob.goto(-480,-350)
+    bob.pendown()
+    for i in range(2):
+        bob.forward(960)
+        bob.left(90)
+        bob.forward(500)
+        bob.left(90)
+    #Write PONG
+    bob.penup()
+    bob.goto(-100,225)
+    bob.pendown()
+    bob.write("PONG",font=("Arial", 75, "normal"))
+    #Draw Scoreboard
+    #Create Pongs
+    ball.speed(0)
+    ball.shape("square")
+    ball.penup()
+    ball.goto(0,-100)
+
+    player1.speed(0)
+    player1.penup()
+    player1.shape("square")
+    player1.shapesize(5,1,None)
+    player1.goto(-460,-100)
+
+    player2.speed(0)
+    player2.penup()
+    player2.shape("square")
+    player2.shapesize(5,1,None)
+    player2.goto(460,-100)
+    
+
+def playGame(bob,gameScreen,player1,player2,ball):    
+    gameScreen.onkey(player1.goto(-460,ycord1+2),"up")
+    gameScreen.listen(None,None)
+
+
+def main():
+    bob = turtle.Turtle()
+    player1 = turtle.Turtle()
+    player2 = turtle.Turtle()
+    ball = turtle.Turtle()
+    gameScreen = turtle.setup(1000,750,startx=0,starty=0)
+    
+    createBoard(bob,gameScreen,player1,player2,ball)
+    playGame(bob,gameScreen,player1,player2,ball)
+    print("Hello World")
+
+main()
